cf2726207c4fec581164238dfb02af0bae41cfca
Ihor <ihorrykun@gmail.com>
2022-08-30 20:47:40 +0300

    Update head.js

--- head.js
+++ head.js
@@ -1,8 +1,42 @@
 
-const wrapper = document.querySelector('.wrapper');
+// const wrapper = document.querySelector('.wrapper');
 
 
-function creatHeader () {
+// function creatHeader () {
+//     return `
+// <header class="header">
+// <h1>Trainerz</h1>
+// <div class="action">
+// <button>Google</button>
+// <button>App Store</button> 
+// </div>
+// </header>`;
+// };
+
+// wrapper.insertAdjacentHTML("beforebegin", creatHeader());
+
+
+// function creatHeroSection() {
+//     return `
+//     <section class="hero">
+//     <h2 class="hero__title">start to workout</h2>
+//     <p class="hero__text">This is a template Figma file,turned into code using Anima.Learn more at AnimaApp.com</p>
+//     <button class="cta">Get started</button>
+//     <img src="https://pic.sport.ua/images/news/0/14/170/middle_594169.jpg" alt="sport" class="hero__workout">
+// </section>`;
+// } 
+
+
+// wrapper.insertAdjacentHTML("beforeend", creatHeroSection());
+
+
+// !Напишемо код у вигляді класу 
+
+class SiteConstructor {
+    constructor() {
+        this.wrapper = document.querySelector('.wrapper');
+    }
+creatHeader () {
     return `
 <header class="header">
 <h1>Trainerz</h1>
@@ -11,12 +45,9 @@ function creatHeader () {
 <button>App Store</button> 
 </div>
 </header>`;
-};
-
-wrapper.insertAdjacentHTML("beforebegin", creatHeader());
-
-
-function creatHeroSection() {
+    };
+    
+    creatHeroSection() {
     return `
     <section class="hero">
     <h2 class="hero__title">start to workout</h2>
@@ -24,7 +55,12 @@ function creatHeroSection() {
     <button class="cta">Get started</button>
     <img src="https://pic.sport.ua/images/news/0/14/170/middle_594169.jpg" alt="sport" class="hero__workout">
 </section>`;
-} 
+    } 
+    render() {
+        this.wrapper.insertAdjacentHTML("beforebegin", this.creatHeader());
+        this.wrapper.insertAdjacentHTML("beforebegin", this.creatHeroSection());
+    }
+};
 
-
-wrapper.insertAdjacentHTML("beforeend", creatHeroSection());
+const site = new SiteConstructor(); 
+site.render();
